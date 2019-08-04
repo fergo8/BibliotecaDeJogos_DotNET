7054af8bd4074d66c2745d3b4fa13cf4110d4ef9
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-08-03 23:31:33 -0300

    Criada classe de Conexão com BD

---
+++ BibliotecaGames.DAL/Conexao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaGames.DAL
+{
+    public class Conexao
+    {
+        static string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConexaoSqlServer"].ConnectionString;
+
+        public static void Conectar()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public static void Desconectar()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
--- BibliotecaGames.DAL/UsuarioDAO.cs
+++ BibliotecaGames.DAL/UsuarioDAO.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BibliotecaGames.Entities;
 
 namespace BibliotecaGames.DAL
 {
-    public class Class1
+    public class UsuarioDAO
     {
+        public Usuario ObterUsuarioPorUsuarioSenha(string usuario, string senha)
+        {
+
+        }
     }
 }
